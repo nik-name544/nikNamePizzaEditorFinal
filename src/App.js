308f3c1f00aede3d1b663169fafd94895cdb796a
nik-name544 <65399330+nik-name544@users.noreply.github.com>
2021-01-13 19:41:20 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,17 +32,6 @@ function App() {
  
   return (
     <>
-//       <nav>
-//         <ul>
-//           <li><Link to="/">PizzaEditor</Link></li>
-//           <li><Link to="/checkout">Checkout</Link></li>
-//           <li><Link to="/registration">Registration</Link></li>
-//           <li><Link to="/login">Login</Link></li>
-//           <li><Link to={auth}>OrderList</Link></li>
-//           <li><Link to="/payment-form">PaymentForm</Link></li>
-//         </ul>
-//       </nav>
-
 
       <Switch>
         <Route path="/" exact>
